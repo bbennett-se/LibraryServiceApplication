b2a7ff4c748044e9bdedf892ef147b6d48a6ec9c
bbenne13 <bbenne13@students.towson.edu>
2026-05-26 18:02:07 -0700

    established circulation service directory structure

---
+++ circulation-service/src/main/java/com/library/circulation/CirculationServiceApplication.java
@@ -0,0 +1,13 @@
+package com.library.circulation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CirculationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CirculationServiceApplication.class, args);
+	}
+
+}
